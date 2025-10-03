e59442c9ff4da2b9e8c0bb233470727cc555830f
caduaquinomorais <eduarduo.aquino@gmail.com>
2025-10-03 15:41:24 -0300

    Configuração models e organização para utilização do Faker

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,39 @@
+from django.utils import timezone
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+#id(primary key - automatico)
+#first_name(string), last_name(string), phone(string)
+#email(email), created_date(date), description(text)
+
+#Implementado depois
+#category(foreign key), show(boolean), owner(foreign key)
+#picture
+
+class Category(models.Model):
+    #Correção do nome plural -> singular
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+    name = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return self.name
+
+class Contact(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone = models.CharField(max_length=50)
+    email = models.EmailField(max_length=254)#blank=True se eu quiser que não seja obrigatório
+    created_date = models.DateTimeField(default=timezone.now)
+    description = models.TextField(blank=True)
+    show = models.BooleanField(default=True)
+    picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m')
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, blank=True, null=True)
+    owner = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True)
+
+    def __str__(self) -> str:
+        return f'{self.id}. {self.first_name} {self.last_name}'
--- project/urls.py
+++ project/urls.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from django.urls import include,path
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('', include('contact.urls')), #Apontando pra url do contact
     path('admin/', admin.site.urls),
 ]
+
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
