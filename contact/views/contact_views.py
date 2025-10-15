4bc56cc3df8fa997819853c8678fd62c42690671
caduaquinomorais <eduarduo.aquino@gmail.com>
2025-10-15 10:47:27 -0300

    Componentização do front e paginação + pesquisa corrigidos

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,13 +1,18 @@
 from django.http import Http404
 from django.shortcuts import get_object_or_404, render,redirect
 from django.db.models import Q
+from django.core.paginator import Paginator
 from contact.models import Contact
 
 def index(request):
-    contacts = Contact.objects.filter(show=True).order_by('-id')[0:10]
+    contacts = Contact.objects.filter(show=True).order_by('-id')
+
+    paginator = Paginator(contacts,10)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
 
     context = {
-        'contacts': contacts,
+        'page_obj': page_obj,
         'site_title': 'Contatos - Agenda' 
     }
 
@@ -20,21 +25,27 @@ def index(request):
 def search(request):
     search_value = request.GET.get('q', '').strip()
 
-    if search_value =='':
+    if search_value == '':
         return redirect('contact:index')
-    
-    contacts = Contact.objects\
+
+    contacts = Contact.objects \
         .filter(show=True)\
         .filter(
-            Q(first_name__icontains=search_value)|
-            Q(last_name__icontains=search_value)|
-            Q(phone__icontains=search_value)|
-            Q(email__icontains=search_value))\
-        .order_by('-id')[:20]
+            Q(first_name__icontains=search_value) |
+            Q(last_name__icontains=search_value) |
+            Q(phone__icontains=search_value) |
+            Q(email__icontains=search_value)
+        )\
+        .order_by('-id')
+
+    paginator = Paginator(contacts, 10)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
 
     context = {
-        'contacts': contacts,
-        'site_title': 'Search - Agenda' 
+        'page_obj': page_obj,
+        'site_title': 'Search - ',
+        'search_value': search_value,
     }
 
     return render(
